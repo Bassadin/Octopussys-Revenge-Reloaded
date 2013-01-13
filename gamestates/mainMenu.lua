19496b6e40388ddce5069b93ac2e2a896702f1c3
Bastian Hodapp <destructive.reality@me.com>
2013-01-13 14:34:50 +0100

    Tiny Commit
    As the title says. I just got the main menu placeholder thingy workin'
    ^^

--- gamestates/mainMenu.lua
+++ gamestates/mainMenu.lua
@@ -1,6 +1,4 @@
---FTR mainMenu.lua
-
-	mainMenu = gamestate.new()
+mainMenu = gamestate.new()
 	
 function mainMenu:init()
 
@@ -13,20 +11,9 @@ end
 function mainMenu:draw()
 
 	--Instruktionen
-		lg.setColor(0, 0, 0, 255)
+		lg.setColor(255, 255, 255, 255)
 		
-		lg.print("PRESS '1' TO START THE GAME", 20, 30)
-		lg.print("PRESS '2' TO VIEW HIGHSCORES", 20, 50)
-		lg.print("PRESS '3' TO VIEW THE CREDITS", 20, 70)
-		lg.print("CLOSE THE GAME USING THE ESCAPE BUTTON", 20, 90)
-		
-		lg.print("INGAME INSTRUCTIONS:", 20, 140)
-		lg.print("- AVOID THE WALLS!", 20, 170)
-		lg.print("- PRESS LEFT AND RIGHT TO MOVE", 20, 190)
-		lg.print("- PRESS 'ESCAPE' TO PAUSE", 20, 210)
-		
-		lg.print("The original game on the TI-84 Plus", screenwidth - 320, screenheight - 20)
-		lg.print("By Destructive Reality, September 2012 - Build "..version, 5, screenheight - 8)
+		lg.print("Octopussy's Revenge Reloaded Awesome Placeholder Text", 20, 30)
 		
 		lg.setColor(255, 255, 255, 255)
 			
